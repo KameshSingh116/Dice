d8c1e5e5deced5a9ac2bcdece9de0f9652eb7ed6
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-05 01:00:34 +0530

    complete

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 #Radhe Radhe Hare Krishn
 import random
+
+choice='y'
+while(choice=='y'):
+    dice=random.randint(1,6)
+    print(dice)
+
+    choice=input("Do you want to continue:(y/n):")
+    if choice=='y':
+        continue
+    else:
+        break
+
+#Radhe Radhe
+#Project complete
